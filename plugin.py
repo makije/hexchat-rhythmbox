dec25cf0cc01e8396d78bd55ed3cfd90b4ca9a59
Martin Kiforenko Jensby <martin@rootuser.dk>
2014-04-28 16:12:10 +0200

    Minor change to the readme, changed the message and format

--- plugin.py
+++ plugin.py
@@ -1,5 +1,5 @@
 __module_name__ = "rhythmbox"
-__module_version__ = "1.1"
+__module_version__ = "1.2"
 __module_description__ = "Plugin for Rhythymbox integration"
 
 import hexchat
@@ -9,11 +9,10 @@ import subprocess
 def np(word, word_eol, userdata):
     output = subprocess.check_output([
         'rhythmbox-client',
-        '--print-playing-format',
-        '%tt - %ta'
+        '--print-playing'
     ])
     output = output.strip()  # Has a trailing newline
-    hexchat.command('me NP: %s' % output)
+    hexchat.command('me is listening to "%s"' % output)
     return hexchat.EAT_ALL
 
 hexchat.hook_command('np', np)
